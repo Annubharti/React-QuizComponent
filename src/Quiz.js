5d52e2130944c76fd8c760f01053a8da323fc259
M1051559 <annu.bharti@mindtree.com>
2021-08-03 17:38:37 +0530

    added rectified module2 solution

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,7 +12,7 @@ class Quiz extends Component{
     render() {
         return (
             
-                <QuizQuestion quiz_question={quizData} /> 
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} /> 
         )
     }
       
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -9,16 +9,14 @@ class QuizQuestion extends Component {
     return (
       <main>
         <section>
-          <p>{ this.props.quiz_question.quiz_questions[0].instruction_text }</p>
+          <p>{ this.props.quiz_question.instruction_text }</p>
         </section>
         <section className="buttons">
           <ul>
-          {console.log(this.props.quiz_question.quiz_questions[0])}
-
-            <li>{this.props.quiz_question.quiz_questions[0].answer_options[0]}</li>
-            <li>{this.props.quiz_question.quiz_questions[0].answer_options[1]}</li>
-            <li>{this.props.quiz_question.quiz_questions[0].answer_options[2]}</li>
-            <li>{this.props.quiz_question.quiz_questions[0].answer_options[3]}</li>
+            <li>{this.props.quiz_question.answer_options[0]}</li>
+            <li>{this.props.quiz_question.answer_options[1]}</li>
+            <li>{this.props.quiz_question.answer_options[2]}</li>
+            <li>{this.props.quiz_question.answer_options[3]}</li>
           </ul>
         </section>
       </main>
